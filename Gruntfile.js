323d9c689acd639cac963e179616e2893e89de6f
simeydotme <simey.me@gmail.com>
2014-04-02 20:50:48 +0100

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,89 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+
+    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+   
+    // Task configuration.
+    concat: {
+      options: {
+        banner: '<%= banner %>',
+        stripBanners: true
+      },
+      dist: {
+        src: ['src/js/jquery-canvas-sparkles.js'],
+        dest: 'dist/jquery-canvas-sparkles.js'
+      }
+    },
+
+
+    uglify: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      dist: {
+        src: '<%= concat.dist.dest %>',
+        dest: 'dist/jquery-canvas-sparkles.min.js'
+      }
+    },
+
+
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        browser: true,
+        globals: {
+          jQuery: true
+        }
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      lib_test: {
+        src: ['src/js/*.js']
+      }
+    },
+
+
+    copy: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      main: {
+        files: [
+          {src: ['src/css/*'], dest: 'dist/', filter: 'isFile', expand: true, flatten: true}        
+        ]
+      }
+    }
+
+
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'copy']);
+
+};
